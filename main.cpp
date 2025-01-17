2e42eb9da6ae9b61d6d4c8155a08a2dea98354be
Pierre Baron <Pierrebaron90@gmail.com>
2025-01-17 12:30:52 +0100

    add random number generator

---
+++ main.cpp
@@ -0,0 +1,68 @@
+/*
+Description:
+This is a random number generator.
+The rand_numbers function generates random numbers using the rand() function.
+The linear_congruential_generator function generates random numbers using the linear congruential generator.
+The rand() function generates random numbers between 0 and RAND_MAX.
+The linear congruential generator generates random numbers between 0 and m-1.
+The linear congruential generator generates a sequence of random numbers that are not truly random,
+it repeats after a certain number of iterations.
+Author: Pierre Baron
+Date: 17/01/2025
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+void rand_numbers(int amount);
+void linear_congruential_generator(int amount, int a, int m, int x0);
+int main()
+{
+    rand_numbers(1000);
+    linear_congruential_generator(1000, 21, 64, 9);
+
+    return 0;
+}
+void rand_numbers(int amount)
+{
+    float x{0.0f};
+    srand(time(NULL));
+    std::ofstream myfile;
+    myfile.open("random_with_rand().txt");
+    if (myfile.is_open())
+    {
+        myfile << "Author: Pierre Baron" << std::endl;
+        for (int i = 0; i < amount; i++)
+        {
+            x = (rand() % 100) / 100.0f;
+            myfile << x << std::endl;
+        }
+        myfile.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file" << std::endl;
+    }
+}
+void linear_congruential_generator(int amount, int a, int m, int x0)
+{
+    std::ofstream myfile;
+    myfile.open("random_with_LCG.txt");
+    if (myfile.is_open())
+    {
+        myfile << "Author: Pierre Baron" << std::endl;
+        for (int i = 0; i < amount; i++)
+        {
+            x0 = (a * x0) % m;
+            myfile << x0 << std::endl;
+        }
+        myfile.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file" << std::endl;
+    }
+}
